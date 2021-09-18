8afe8fa7ddd01957ca1266fc6665a8294d59911a
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-09-18 14:20:59 +0200

    Welcome View

---
+++ Shared/Welcome/WelcomeView.swift
@@ -0,0 +1,48 @@
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        ZStack {
+            Image("welcome-background", bundle: nil)
+                .resizable()
+                .aspectRatio(1, contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+                .saturation(0.5)
+                .blur(radius: 5)
+                .opacity(0.08)
+            
+            HStack {
+                Image(systemName: "table")
+                  .resizable()
+                  .frame(width: 60, height: 60, alignment: .center)
+                  .border(Color.gray, width: 1)
+                  .cornerRadius(60 / 2)
+                  .background(Color(white: 0.9))
+                  .clipShape(Circle())
+                    .foregroundColor(.red)
+                
+                VStack {
+                    Text("Welcome to")
+                      .font(.headline)
+                      .bold()
+                    
+                    Text("Kuchi")
+                        .font(.largeTitle)
+                      .bold()
+                }
+                .foregroundColor(.red)
+                .lineLimit(2)
+                .multilineTextAlignment(.leading)
+                .padding(.horizontal)
+
+            }
+        }
+        
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
