c79076d96b6b7e01a68ebbdfe0edec1072636f63
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-09-24 20:04:53 +0200

    Refactor

--- Shared/Welcome/WelcomeView.swift
+++ Shared/Welcome/WelcomeView.swift
@@ -3,36 +3,11 @@ import SwiftUI
 struct WelcomeView: View {
     var body: some View {
         ZStack {
-            Image("welcome-background", bundle: nil)
-                .resizable()
-                .aspectRatio(1, contentMode: .fill)
-                .edgesIgnoringSafeArea(.all)
-                .saturation(0.5)
-                .blur(radius: 5)
-                .opacity(0.08)
-            
-            Label { // 1
-                VStack(alignment: .leading) {
-                    Text("Welcome to")
-                        .font(.headline)
-                        .bold()
-                    Text("Kuchi")
-                        .font(.largeTitle)
-                        .bold() }
-                    .foregroundColor(.red)
-                    .lineLimit(2)
-                    .multilineTextAlignment(.leading)
-                    .padding(.horizontal)
-                // 2
+            WelcomeBackgroundImage()
+            Label {
+                LogoImage()
             } icon: {
-                // 3
-                Image(systemName: "table")
-                    .resizable()
-                    .frame(width: 30, height: 30)
-                    .overlay(Circle().stroke(Color.gray, lineWidth: 1))
-                    .background(Color(white: 0.9))
-                    .clipShape(Circle())
-                    .foregroundColor(.red)
+               WelcomeMessageView()
             }.labelStyle(HorizontallyAlignedLabelStyle()) // Label
         } // Zstack
         
