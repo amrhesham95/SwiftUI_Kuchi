6a54d473d5d199f1c4119b5e1c99330117c2cc70
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-10-13 15:19:13 +0200

    Reacting to input: validation

--- Shared/Welcome/RegisterView.swift
+++ Shared/Welcome/RegisterView.swift
@@ -24,6 +24,7 @@ struct RegisterView: View {
                 }
             })
             .bordered()
+            .disabled(!userManager.isUserNameValid())
             
             Spacer()
           }.padding()
